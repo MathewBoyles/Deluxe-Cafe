aeb4d16ccbf850a05db5f4e0bda7ccca82c13583
Mathew Boyles <hello@mathewboyles.com>
2017-04-03 22:08:00 +1200

    Started Project

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $(document).on('scroll',function(){
+    if($(window).scrollTop() > 20) $('#navbar').addClass('navbar-scroll');
+    else $('#navbar').removeClass('navbar-scroll');
+    $('.content').each(function(){
+      if($(window).scrollTop() >= ($(this).offset()['top']-($(window).height()/3))) $(this).addClass('active');
+      else $(this).removeClass('active');
+    });
+    $('#fscroll > li.active').removeClass('active');
+    $('#fscroll > li > a[data-fscroll="#'+$('.content.active:last').attr('id')+'"]').parent().addClass('active');
+    if($('.content:not(.active)').is('*')) $('#scrolltip').removeClass('reverse');
+    else $('#scrolltip').addClass('reverse');
+  }).trigger('scroll');
+  $('[data-fscroll]').click(function(){
+    if($($(this).attr('data-fscroll')).is('*')) $('body').animate({scrollTop:($($(this).attr('data-fscroll')).offset()['top'])},1000);
+    return false;
+  });
+  $('#scrolltip').click(function(){
+    $('body').animate({scrollTop: $(this).hasClass('reverse')?0:($('.content:not(.active):first').offset()['top']) },1000);
+  });
+});
