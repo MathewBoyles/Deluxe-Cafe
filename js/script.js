43e7203d43ba99aff8298fad4faed10cc6c1f9e7
Mathew Boyles <hello@mathewboyles.com>
2017-04-05 18:39:53 +1200

    Added fixed-scroll functionality
    Page scroll will adhere to content position.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+  var previous_scroll = 0;
+  $('body').addClass('scrolling').scrollTop(0);
+  setTimeout(function(){
+    $('body').removeClass('scrolling');
+  },100);
   $(document).on('scroll',function(){
     if($(window).scrollTop() > 20) $('#navbar').addClass('navbar-scroll');
     else $('#navbar').removeClass('navbar-scroll');
@@ -6,17 +11,39 @@ $(document).ready(function(){
       if($(window).scrollTop() >= ($(this).offset()['top']-($(window).height()/3))) $(this).addClass('active');
       else $(this).removeClass('active');
     });
-    $('.navbar > .navbar_social > li.active').removeClass('active');
-    $('.navbar > .navbar_social > li > a[data-fscroll="#'+$('.content.active:last').attr('id')+'"]').parent().addClass('active');
+    $('.navbar > .navbar_links > li.active').removeClass('active');
+    $('.navbar > .navbar_links > li > a[data-fscroll="#'+$('.content.active:last').attr('id')+'"]').parent().addClass('active');
     if($('.content:not(.active)').is('*')) $('#scrolltip').removeClass('reverse');
     else $('#scrolltip').addClass('reverse');
+    if(!$('body').hasClass('scrolling')){
+      var do_scrolltop = ($('body').scrollTop()>previous_scroll) ? $('.content:not(.active):first') : $('.content.active:eq('+($('.content.active').length-2)+')');
+      if(do_scrolltop.is('*')){
+        $('body').addClass('scrolling').animate({scrollTop:do_scrolltop.offset()['top']},1000,function(){
+          setTimeout(function(){
+            $('body').removeClass('scrolling');
+          },100);
+        });
+      }
+      return false;
+    }
+    previous_scroll = $('body').scrollTop();
   }).trigger('scroll');
   $('[data-fscroll]').click(function(){
-    if($($(this).attr('data-fscroll')).is('*')) $('body').animate({scrollTop:($($(this).attr('data-fscroll')).offset()['top'])},1000);
+    if($($(this).attr('data-fscroll')).is('*')){
+      $('body').addClass('scrolling').animate({scrollTop:($($(this).attr('data-fscroll')).offset()['top'])},1000,function(){
+        setTimeout(function(){
+          $('body').removeClass('scrolling');
+        },100);
+      });
+    }
     return false;
   });
   $('#scrolltip').click(function(){
-    $('body').animate({scrollTop: $(this).hasClass('reverse')?0:($('.content:not(.active):first').offset()['top']) },1000);
+    $('body').addClass('scrolling').animate({scrollTop: $(this).hasClass('reverse')?0:($('.content:not(.active):first').offset()['top']) },1000,function(){
+      setTimeout(function(){
+        $('body').removeClass('scrolling');
+      },100);
+    });
   });
   $('[data-modal]').click(function(){
     $('#modal-backdrop').fadeIn();
@@ -26,5 +53,4 @@ $(document).ready(function(){
     $(this).fadeOut();
     $('.modal:visible').fadeOut();
   });
-
 });
